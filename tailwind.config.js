b648404daa7fdec85b3b78883eb0dac8c548d5ce
sheetal <sheetal.kulkarni91@gmail.com>
2023-03-27 17:31:46 -0700

    updated pattern validation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,17 @@ module.exports = {
     extend: {
       backgroundImage: {
         'card-desktop': "url('./images/bg-main-desktop.png')",
-        'card-mobile': "url('./images/bg-main-mobile.png')"
+        'card-mobile': "url('./images/bg-main-mobile.png')",
+        'card-front-design': "url('./images/bg-card-front.png')",
+        'card-back-design': "url('./images/bg-card-back.png')",
       },
       fontSize: {
         'sm': '0.5em'
+      },
+      colors: {
+        'left': '#6448fe',
+        'right': '#600594',
+        'error': 'hsl(0, 100%, 66%)'
       }
     },
   },
